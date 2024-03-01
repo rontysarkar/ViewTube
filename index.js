e7b1af37e2407767a7bbef8acb97c2e3a348ad42
rontysarkar <rontysarkar2023@gmail.com>
2024-03-01 17:53:05 +0600

    addd

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const btnContainer = document.getElementById('btn-container');
 const videoCardContainer = document.getElementById('video-card-container');
+const erroElement = document.getElementById("erro-element")
+
 
 let selectedCategory = 1000;
+let sortBtns = false;
+
+function sortBtn(){
+  sortBtns = true;
+  videoCardInfo(selectedCategory,sortBtns)
+}
 
 const fetchCategoris = () => {
     fetch('https://openapi.programming-hero.com/api/videos/categories')
@@ -9,28 +17,58 @@ const fetchCategoris = () => {
         .then(({data}) => {
          data.forEach(btn => {
              const newBtn = document.createElement('button');
-             newBtn.classList = `btn px-6 border-none`
+             newBtn.classList = `btn-category btn px-6 border-none`
              newBtn.innerText = btn.category;
+             newBtn.classList.remove("bg-red-600")
              btnContainer.appendChild(newBtn)
+             
+  
             //  console.log(data)
 
-             newBtn.addEventListener('click', () => {videoCardInfo(btn.category_id)})
+             newBtn.addEventListener('click', () => {
+              let btnCategory = document.querySelectorAll(".btn-category")
+              btnCategory.forEach((btns) =>{
+                btns.classList.remove('bg-red-600')
+              })
+              newBtn.classList.add('bg-red-600')
+              
+              
+              videoCardInfo(btn.category_id)})
          });
 
         })
         
 }
 
-const videoCardInfo = (category_id) =>{
+const videoCardInfo = (category_id ,sortBtns) =>{
     selectedCategory = category_id;
     fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
         .then((res) =>res.json())
         .then(({data}) => {
+          if(sortBtns){
+            data.sort((a,b) =>{
+              const fastView = a.others?.views;
+              const secoundView = b.others?.views;
+              const fastViewNumber = parseFloat(fastView.replace("K",'')) || 0;
+              const secoundViewNumber = parseFloat(secoundView.replace("K","")) || 0;
+              return secoundViewNumber - fastViewNumber;
+    
+            })
+          }
             
-           
+            if(data.length === 0){
+              erroElement.classList.remove('hidden')
+            }else{
+              erroElement.classList.add('hidden')
+            }
             videoCardContainer.innerHTML = "";
             data.forEach((cardInfo) => {
-                console.log(cardInfo)
+    
+                // console.log(cardInfo.authors[0].verified)
+                let verifiedBadge = ''
+                if(cardInfo.authors[0].verified){
+                  verifiedBadge = `<img class="size-6" src="image/blue.png" alt="">`
+                }
                 const div = document.createElement('div');
                 div.innerHTML = `
                 <div class="card card-compact w-[90%] bg-base-100 shadow-xl">
@@ -47,7 +85,7 @@ const videoCardInfo = (category_id) =>{
                     <h2 class="card-title ">${cardInfo.title}</h2>
                     <div class="flex gap-4 my-3">
                       <p class="text-xl">Awlad Hossain</p>
-                      <img class="size-6" src="image/blue.png" alt="">
+                      ${verifiedBadge}
                     </div>
                     <p id="views" class="text-xl pb-5">${cardInfo.others.views} views</p>
 
